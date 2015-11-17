17ba6e77d2cc789dc7e3815de49ee6471d075e1d
Khyzad <lljruffin@gmail.com>
2015-11-17 02:59:23 -1000

    -cleaned up code -added javadocs -still need to implement data integrity -still need to implement uniqueness

--- src/main/java/com/hida/DatabaseManager.java
+++ src/main/java/com/hida/DatabaseManager.java
@@ -8,31 +8,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
- * A template for all minters should inherit from.
- *
- * As of right now tentative functionality includes...
- * <p>
- * - storing and retrieving user preferences (maybe with java beans?)
- * </p>
- * <p>
- * - connecting to database via connectDb method
- * </p>
- * <p>
- * - a check id method that will check a given id against a database
- * </p>
- * <p>
- * - implementation of a gen id method
- * </p>
- *
- * Currently deciding whether or not to merge Arkminter and this class into a
- * single class because, as far as I can tell, ids will always be generated in
- * the same, random or sequential. Ids differ only slightly from schema to
- * schema
+ * A class used to manage http requests so that data integrity can be
+ * maintained in the database
  *
  * @author lruffin
  */
@@ -44,22 +24,11 @@ public class DatabaseManager {
     private final String DRIVER = "jdbc:sqlite:PID.db";
     private boolean isTableCreatedFlag = false;
 
+    
     /**
-     * Proposed use to retrieve any stored data from database. Deciding how
-     * it'll interact with java beans. Need to read up more on it
-     */
-    /*
-     public void retrieveSettings() {
-     this.charMap = "12345abc";
-     this.idPrefix = "xyz1";
-     this.idLength = 10;
-     }
-     */
-    /**
-     * Proposed use is to connect to a database so that ids can be checked and
-     * user settings can be retrieved
+     * Attempt to connect to the database.
      *
-     * @return - true if connection is successful
+     * @return - true if connection is successful, false otherwise
      */
     public synchronized boolean createConnection() {
         try {
@@ -98,7 +67,7 @@ public class DatabaseManager {
      * null pointer if an error was found.
      */
     public synchronized ArrayList<String> addId(ArrayList<String> list) {
-        ArrayList<String> redundantIdList = new ArrayList();
+        
         try {
 
             // a string to query a specific id
@@ -184,38 +153,5 @@ public class DatabaseManager {
             return false;
         }
     }
-
-    /**
-     * Checks a given id against a database.
-     *
-     * Perhaps another method to overload this can be used to check a list of
-     * ids.
-     *
-     * @param id - given id
-     * @return - true if id is true
-     */
-    public boolean checkId(String id) {
-
-        return true;
-    }
-
-    /**
-     * A method to overwritten by a subclass. This method will automatically
-     * generate ids according a received token. More information on the token in
-     * the ArkMinters implementation
-     *
-     * @param token - decides how ids are minted
-     * @return - A reference to a JsonObject that contains Json list of ids
-     */
-    //public abstract JsonObject genIdAuto(String token);
-    /**
-     * A method to overwritten by a subclass. This method will generate ids
-     * sequentially or randomly according the charMapping given by user
-     *
-     * @param isRandom - if true then create ids randomly
-     * @return - A reference to a JsonObject that contains Json list of ids
-     */
-    //public abstract JsonObject genIdCustom(boolean isRandom);
-
-    /* typical getter and setter methods */
+       
 }
