7a4368170277129c51d1ffe26a9853718fe4eca6
Khyzad <lljruffin@gmail.com>
2015-11-19 07:34:19 -1000

    -fixed genCustomId method -need to clean up javadocs -need to fix the 'potential' infinite loop with random generation. Explained in javadoc

--- src/main/java/com/hida/Minter.java
+++ src/main/java/com/hida/Minter.java
@@ -18,6 +18,7 @@ public class Minter {
     private final static DatabaseManager DATABASE_MANAGER
             = new DatabaseManager();
     private final String CHAR_MAP;
+    private final HashMap<Character, String> BASE_MAP = new HashMap();
 
     // fields    
     private final String NAAN = "/:ark/70111/";
@@ -56,14 +57,22 @@ public class Minter {
         this.tokenMaps.put("ALPHANUMERIC",
                 "0123456789abcdefghijklmnopqrstuvwxyz"
                 + "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+        this.BASE_MAP.put('d', "DIGIT");
+        this.BASE_MAP.put('l', "LOWERCASE");
+        this.BASE_MAP.put('u', "UPPERCASE");
+        this.BASE_MAP.put('m', "MIXCASE");
+        this.BASE_MAP.put('e', "EXTENDED");
+        this.BASE_MAP.put('a', "ALPHANUMERIC");
+
     }
 
     /**
      * genIDauto() Method description: This generates an automated ID format.
      * Note that this method will take longer and longer to create unique ids
-     * because the ids it produces will randomize. Need to figure out a way
-     * to quickly discover unminted ids in database. 
-     * 
+     * because the ids it produces will randomize. Need to figure out a way to
+     * quickly discover unminted ids in database.
+     *
      * User would give a string that excepts 6 letters: {d, l, u, m, e, a}.
      * Similar to the Mode settings in genIDauto() method. d:
      *
@@ -84,11 +93,11 @@ public class Minter {
      * @param token: There are 2 different modes to choose from. RANDOM: Value
      * by value, each value's order is randomized. SEQUENTIAL: Value by value,
      * each value's order is sequenced.
-     * @return - The method checks to see if it is even possible to produce
-     * the requested amount of ids using the given parameters. If it can't 
-     * it'll return an error message. Otherwise a reference to a JsonObject 
-     * that contains Json list of ids is returned. 
-     * 
+     * @return - The method checks to see if it is even possible to produce the
+     * requested amount of ids using the given parameters. If it can't it'll
+     * return an error message. Otherwise a reference to a JsonObject that
+     * contains Json list of ids is returned.
+     *
      */
     public String genIdAuto(String token) {
 
@@ -97,7 +106,7 @@ public class Minter {
         if (isPrefixAvailable(true, token)) {
             // creates a list to hold redundant ids
             Set<String> redundantIdList = new HashSet();
-            
+
             // a range of characters that can be used, retrieved by tokenMap
             String characters = (String) this.tokenMaps.get(token);
 
@@ -105,11 +114,11 @@ public class Minter {
             int numIdsToMake = this.AMOUNT;
             int prefixSize = this.PREFIX.length();
             int charactersLength = characters.length();
-            
+
             do {
-                
+
                 // creates tentative ids that'll be checked for uniqueness
-                while(ID_LIST.size() < numIdsToMake) {
+                while (ID_LIST.size() < AMOUNT) {
                     StringBuilder buffer = new StringBuilder();
                     for (int i = 0; i < this.LENGTH - prefixSize; i++) {
                         double index = Math.random() * charactersLength;
@@ -122,17 +131,16 @@ public class Minter {
 
                 // removes any redundant ids
                 removeRedundantId(redundantIdList);
-                
+
                 // if no redundant ids were found, add new ids to database
-                if(ID_LIST.size() == this.AMOUNT){
+                if (ID_LIST.size() == this.AMOUNT) {
                     DATABASE_MANAGER.addId(ID_LIST);
-                }else{     
+                } 
                     // decreases the number of ids to make based on what was 
                     // just added to ID_LIST
                     numIdsToMake = this.AMOUNT - ID_LIST.size();
-                }
                 
-
+                System.out.println("numIdsToMake = " + numIdsToMake);
                 System.out.println("size = " + ID_LIST.size());
                 System.out.println("amount = " + this.AMOUNT);
             } while (ID_LIST.size() < this.AMOUNT);
@@ -149,51 +157,65 @@ public class Minter {
     /**
      * A method used to see the amount of ids in the database contain the
      * requested prefix and length.
-     * 
-     * There is a flaw in that this method does not use big integer. As of
-     * right now it depends on user not choosing a format that will not produce
-     * 2^64 unique ids. 
+     *
+     * There is a flaw in that this method does not use big integer. As of right
+     * now it depends on user not choosing a format that will not produce 2^64
+     * unique ids.
      *
      * @param isAuto
      * @param token
      * @return - true if there is space available
      */
     private boolean isPrefixAvailable(boolean isAuto, String token) {
-        
+
         int matchingIds = DATABASE_MANAGER.checkPrefix(PREFIX, LENGTH);
         int numFreeDigits = this.LENGTH - this.PREFIX.length();
-        int base = this.tokenMaps.get(token).length();
-        
-        if(matchingIds == -1){
+
+        if (matchingIds == -1) {
             System.out.println("somethings wrong in isPrefix");
             return false;
         }
 
         if (isAuto) {
+            int base = this.tokenMaps.get(token).length();
+
             // counts the number of permutations
             long numPermutations = (long) Math.pow(base, numFreeDigits);
             return numPermutations - matchingIds > this.AMOUNT;
         } else {
-            return false;
+            long numPermutations = 1;
+            for (int i = 0; i < CHAR_MAP.length(); i++) {
+                /* when it isn't auto, we will not check the database as that
+                 will not produce an accurate reseult. What will be returned 
+                 instead is the comparison between the maximum number of 
+                 permutations and the requested amount.
+                 */
+
+                String charToken = BASE_MAP.get(CHAR_MAP.charAt(i));
+                int base = tokenMaps.get(charToken).length();
+                numPermutations *= base;
+            }
+
+            return numPermutations >= AMOUNT;
         }
 
     }
 
     /**
      * Removes redundant ids from ID_LIST based on given redundantList.
-     * 
+     *
      * @param redundantList - list of redundancies
      */
     private void removeRedundantId(Set<String> redundantList) {
         for (String redundantId : redundantList) {
             ID_LIST.remove(redundantId);
-            System.out.println("removed: " + redundantId);            
+            System.out.println("removed: " + redundantId);
         }
         System.out.println("printing current list of ids");
         for (String id : ID_LIST) {
-            
+
             System.out.println(id);
-        }        
+        }
     }
 
     /**
@@ -215,104 +237,157 @@ public class Minter {
      * ALPHANUMERIC: {012...abc...XYZ} Digit values, Lowercase and Uppercase
      * letters.</p>
      *
-     * @param isRandom - generates ids based on whether or not user requested
+     * @param isSequential - generates ids based on whether or not user requested
      * random strings
      * @return - A reference to a JsonObject that contains Json list of ids
      */
-    public String genIdCustom(boolean isRandom) {
-        String format = this.CHAR_MAP.toLowerCase();
-        String tokenType = "";
-        double index = 0.0D;
-        for (int j = 0; j < this.AMOUNT; j++) {
-            StringBuilder buffer = new StringBuilder();
-            for (int i = 0; i < format.length(); i++) {
-                char c = format.charAt(i);
-                //if (isRandom) {
-                switch (c) {
-                    case 'd':
-                        index = (isRandom) ? Math.random() * 10.0D : index + 1.0D;
-                        tokenType = "DIGIT";
-                        //buffer.append(((String) this.tokenMaps.get("DIGIT")).charAt((int) index));
-                        break;
-                    case 'l':
-                    case 'u':
-                        index = (isRandom) ? Math.random() * 26.0D : index + 1.0D;
-                        //index = Math.random() * 26.0D;
-                        //buffer.append(((String) this.tokenMaps.get("LOWERCASE")).charAt((int) index));
-                        tokenType = "LOWERCASE";
-                        break;
-                    /*
-                     case 'u':
-                     index = (isRandom)? Math.random() * 10.0D : index + 1.0D;
-                     index = Math.random() * 26.0D;
-                     //buffer.append(((String) this.tokenMaps.get("UPPERCASE")).charAt((int) index));
-                     tokenType = "UPPERCASE";
-                     break;
-                     */
-                    case 'm':
-                        index = (isRandom) ? Math.random() * 52.0D : index + 1.0D;
-                        //index = Math.random() * 52.0D;
-                        //buffer.append(((String) this.tokenMaps.get("MIXCASE")).charAt((int) index));
-                        tokenType = "MIXCASE";
-                        break;
-                    case 'e':
-                        index = (isRandom) ? Math.random() * 36.0D : index + 1.0D;
-                        //index = Math.random() * 36.0D;
-                        //buffer.append(((String) this.tokenMaps.get("EXTENDED")).charAt((int) index));
-                        tokenType = "EXTENDED";
-                        break;
-                    case 'a':
-                        index = (isRandom) ? Math.random() * 62.0D : index + 1.0D;
-                        //index = Math.random() * 62.0D;
-                        //buffer.append(((String) this.tokenMaps.get("ALPHANUMERIC")).charAt((int) index));
-                        tokenType = "ALPHANUMERIC";
-                        break;
-                }
-                buffer.append(
-                        (this.tokenMaps.get(tokenType)).charAt((int) index));
+    public String genIdCustom(boolean isSequential) {
+        if (isPrefixAvailable(false, null)) {
 
-                /*} else {
-                 switch (c) {
-                 case 'd':
-                 //buffer.append(((String) this.tokenMaps.get("DIGIT")).charAt((int) index));
-                 tokenType = "DIGIT";
-                 index += 1.0D;
-                 break;
-                 case 'l':
-                 //buffer.append(((String) this.tokenMaps.get("LOWERCASE")).charAt((int) index));
-                 tokenType = "LOWERCASE";
-                 index += 1.0D;
-                 break;
-                 case 'u':
-                 //buffer.append(((String) this.tokenMaps.get("UPPERCASE")).charAt((int) index));
-                 tokenType = "UPPERCASE";
-                 index += 1.0D;
-                 break;
-                 case 'm':
-                 //buffer.append(((String) this.tokenMaps.get("MIXCASE")).charAt((int) index));
-                 tokenType = "MIXCASE";
-                 index += 1.0D;
-                 break;
-                 case 'e':
-                 //buffer.append(((String) this.tokenMaps.get("EXTENDED")).charAt((int) index));
-                 tokenType = "EXTENDED";
-                 index += 1.0D;
-                 break;
-                 case 'a':
-                 //buffer.append(((String) this.tokenMaps.get("ALPHANUMERIC")).charAt((int) index));
-                 tokenType = "ALPHANUMERIC";
-                 index += 1.0D;
-                 break;
-                 }
-                 }
-                 */
-                buffer.append(
-                        (this.tokenMaps.get(tokenType)).charAt((int) index));
-            }
-            String temp = buffer.toString();
-            this.ID_LIST.add(this.PREFIX + temp);
+            int[] baseMap = new int[CHAR_MAP.length()];
+            
+            // creates a list to hold redundant ids
+            Set<String> redundantIdList;
+
+            // sets the number of ids to make
+            int numIdsToMake = this.AMOUNT;
+            long maxIdsToMake = calculateCharMapPermutations();
+            
+            // continue making ids until the size of the list reaches the 
+            // requested amount.
+            do{
+                if(isSequential){ // if the sequential option was chosen
+                    baseMap = sequentialAlgorithm(numIdsToMake, baseMap);
+                }else{ // if random option was chosen
+                    randomAlgorithm(numIdsToMake);
+                }
+                redundantIdList = DATABASE_MANAGER.checkId(ID_LIST);
+                    removeRedundantId(redundantIdList);
+
+                    // if no redundant ids were found, add new ids to database
+                    if (ID_LIST.size() == this.AMOUNT) {
+                        DATABASE_MANAGER.addId(ID_LIST);
+                    } else if (maxIdsToMake == redundantIdList.size()) {
+                        return "cannot make any more ids given parameters1";
+                    } else {
+                        // decreases the number of ids to make based on what was 
+                        // just added to ID_LIST
+                        numIdsToMake = this.AMOUNT - ID_LIST.size();
+                    }
+                } while (ID_LIST.size() < this.AMOUNT);
+            return convertListToJson();
+        } else {
+            return "cannot make any more ids given parameters";
         }
-        return convertListToJson();
+
+    }
+
+    /**
+     * Calculates the number of possible permutations in a given char mapping
+     * @return - number of permutations
+     */
+    private long calculateCharMapPermutations() {
+        long numPermutations = 1;
+        for (int i = 0; i < CHAR_MAP.length(); i++) {
+            /* when it isn't auto, we will not check the database as that
+             will not produce an accurate reseult. What will be returned 
+             instead is the comparison between the maximum number of 
+             permutations and the requested amount.
+             */
+
+            String charToken = BASE_MAP.get(CHAR_MAP.charAt(i));
+            System.out.println("charToken = " + charToken);
+            int base = tokenMaps.get(charToken).length();
+            System.out.println("base = " + base);
+            numPermutations *= base;
+            System.out.println("numPermutations = " + numPermutations);
+        }
+        return numPermutations;
+    }
+    
+    
+    /**
+     * Generates random ids based on char mappings. The ids are stored in
+     * ID_LIST
+     * @param numIdsToMake - number of ids to make     
+     */
+    private void randomAlgorithm(int numIdsToMake){          
+        String tempId, charToken;
+        String charRange;
+        int randomNum;
+                
+        while (ID_LIST.size() < numIdsToMake) {
+            tempId = PREFIX; 
+            for (int j = 0; j < CHAR_MAP.length(); j++) {
+                charToken = BASE_MAP.get(CHAR_MAP.charAt(j));
+                charRange = tokenMaps.get(charToken);
+                randomNum = (int)(Math.random() * charRange.length());
+                
+                tempId += charRange.charAt(randomNum);
+                                                
+            }
+            ID_LIST.add(tempId);
+        }
+                
+    }
+
+    /**     
+     * In the for loop, k represents last index of char mapping. This for 
+     * loop will incrementally increase the last digit until it reaches its
+     * max value, designated by CHAR_MAP and BASE_MAP. If there's 
+     * overflow, k is incremented so that the next digit can also be
+     * incremented. This process continues until there is no overflow. The
+     * last sequence is returned to the caller. 
+     * @param numIdsToMake 
+     * @param baseMap
+     */
+    private int[] sequentialAlgorithm(int numIdsToMake, int[] baseMap) {
+        
+        StringBuilder buffer = new StringBuilder();
+        
+
+        // define the last position of the map
+        int lastPos = CHAR_MAP.length() - 1;
+
+        buffer.append(PREFIX);
+
+        // prepare storing the number of ids that will be created
+        int idsCreated = 0;
+        
+        for (int k = 0; idsCreated < numIdsToMake;) {
+            // increment the last digit - k
+            baseMap[lastPos - k]++;
+
+            // store the value of the digit in question
+            int value = baseMap[lastPos - k];
+            String token = BASE_MAP.get(CHAR_MAP.charAt(lastPos - k));
+
+            // if the max value of a particular digit is reached, overflow
+            if (value == tokenMaps.get(token).length()) {
+                baseMap[lastPos - k] = 0;
+                k++;
+            } else {
+                String tempId = PREFIX;
+
+                for (int i = 0; i < CHAR_MAP.length(); i++) {
+                    char character = CHAR_MAP.charAt(i);
+                    String charToken = BASE_MAP.get(character);
+                    String map = tokenMaps.get(charToken);
+
+                    tempId += map.charAt(baseMap[i]) + "";
+                }
+                System.out.println("tempid = " + tempId);
+                // add id to list
+                ID_LIST.add(tempId);
+
+                // if there wasn't over flow, increase id count
+                idsCreated++;
+
+                // reset k so it'll point to last digit again
+                k = 0;
+            }
+        }
+        return baseMap;
     }
 
     /**
@@ -328,13 +403,15 @@ public class Minter {
         try {
             // Object used to iterate through list of ids
             Iterator<String> iterator = ID_LIST.iterator();
-            for (int i = 0;iterator.hasNext(); i++) {
-                
+            for (int i = 0; iterator.hasNext(); i++) {
+
                 // map desired Json format
                 String id = String.format(
                         "{\"id\":%d,\"name\":\"%s\"}", i, iterator.next());
 
-                formattedJson = mapper.readValue(id, Object.class);
+                formattedJson
+                        = mapper.readValue(id, Object.class
+                        );
 
                 // append formatted json
                 jsonString += mapper.writerWithDefaultPrettyPrinter().
@@ -381,7 +458,7 @@ public class Minter {
 
     public Set<String> getID_ARRAY() {
         return ID_LIST;
-    }   
+    }
 
     public DatabaseManager getDatabaseManager() {
         return DATABASE_MANAGER;
